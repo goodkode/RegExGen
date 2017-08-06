98fef1bfb089b8939d3c4660160a963db8415a43
Oliver Takacs <olivertakacs@olivertakacs-macbookair.roam.corp.google.com>
2017-08-05 22:19:02 -0400

    refctor dimens

--- app/src/main/java/net/takoli/regexgen/DisplayCheatSheetActivity.java
+++ app/src/main/java/net/takoli/regexgen/DisplayCheatSheetActivity.java
@@ -3,7 +3,6 @@ package net.takoli.regexgen;
 import android.app.Activity;
 import android.app.DialogFragment;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
@@ -16,16 +15,9 @@ public class DisplayCheatSheetActivity extends Activity {
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.cheat_sheet_table);
 	}
-	
-	@Override
-	// Menu about
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true; }
+
     @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-		//Log.i("about", "pressed");
+    public boolean onOptionsItemSelected(MenuItem item) {
 		DialogFragment about = new AboutDialogFragment();
 		about.show(getFragmentManager(), "test");
 		return true;
@@ -33,7 +25,6 @@ public class DisplayCheatSheetActivity extends Activity {
 	
 	@Override
 	public void onBackPressed() {
-		//Log.i("cheatSheet", "onBackPressed");
 		finish();
 		overridePendingTransition(R.anim.main_return, R.anim.cheat_exit);
 	}
